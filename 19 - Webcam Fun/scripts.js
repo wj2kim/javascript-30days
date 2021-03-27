1dac71d7a9dccb2e7c92273f265e0632d5897c73
김우정 <wjkim@rsupport.com>
2021-03-27 15:47:59 +0900

    lesson 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,33 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+async function getVideo() {
+    try{
+        const localMediaStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+        console.log('localMediaStream', localMediaStream);
+        video.srcObject = localMediaStream;
+        video.play();
+    }catch(err) {
+        alert('Cannot get Stream', err);
+    }
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+}
+
+getVideo();
