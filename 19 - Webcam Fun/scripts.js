8a06df5260826a1bf64ca7009fcd47a2e9fe9b61
김우정 <wjkim@rsupport.com>
2021-03-28 23:59:13 +0900

    lesson19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,12 +24,37 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        pixels = redEffect(pixels);
+
     }, 16);
 }
 
 function takePhoto() {
     snap.currentTime = 0;
     snap.play();
+
+    // handle data 
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
 }
 
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.length; i +=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; //Red
+        
+        pixels.data[i + 2] = pixels.data[i + 1] - 50; // Green
+
+        pixels[i + 2]
+    }
+}
+
+
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
